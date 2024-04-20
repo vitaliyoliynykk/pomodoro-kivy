41ac63454cfe8f6179abce9a24f1c06e4dd4bc4e
Vitalii <vitaliyoliynykk@gmail.com>
2024-04-20 10:09:10 +0300

    layout

--- components/circle_progress_bar.py
+++ components/circle_progress_bar.py
@@ -5,13 +5,8 @@ from kivymd.uix.anchorlayout import MDAnchorLayout
 
 Builder.load_file('components/circle_progress_bar.kv')
 
-def hex_to_rgb(value, alpha=1):
-    value = value.lstrip('#')
-    return list(int(value[i:i+2], 16) / 255.0 for i in (0, 2, 4)) + [alpha]
-
 class CircleProgressBar(MDAnchorLayout):
     bar_color = ListProperty([163/255, 67/255, 67/255])
-    # bar_color_secondary = ListProperty(hex_to_rgb('#A34343'))
 
     bar_width = NumericProperty(10)
 
--- screens/timer.py
+++ screens/timer.py
@@ -1,50 +1,79 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.clock import Clock
-from components.circle_progress_bar import CircleProgressBar
 from kivy.lang import Builder
+from kivy.properties import ListProperty
+from kivy.properties import BooleanProperty
+
 
 
 Builder.load_file('screens/timer.kv')
 
-# from .progress_bar import SemiCircleProgressBar
-
 class TimerWidget(GridLayout):
-    def __init__(self, max_time=1, **kwargs):
+    sequence = [{'time': 25, 'type': 'focus'}, 
+                {'time': 5, 'type': 'rest'},
+                {'time': 25, 'type': 'focus'},
+                {'time': 5, 'type': 'rest'},
+                {'time': 25, 'type': 'focus'},
+                {'time': 15, 'type': 'rest'}]
+    
+    primary_color = ListProperty([163/255, 67/255, 67/255])
+    skip_disabled = BooleanProperty(True)
+
+    def __init__(self, max_time=15, **kwargs):
         super().__init__(**kwargs)
-        self.max_time = max_time * 60  # Convert to seconds
+        self.iteration = 0
+        self.max_iteration = 5
+
+        self.set_max_time()
         self.current_time = self.max_time
         self._timer_event = None
-        self.timer_label = '00:00'
+        self.skip_disabled = True
 
-    def start_timer(self, instance):
+
+    def toggle_timer(self):
         if self._timer_event:
             self._timer_event.cancel()
             self._timer_event = None
+            self.ids.start_button.text = 'Start'
         else:
             self._timer_event = Clock.schedule_interval(self.update_timer, 1)
-        instance.set_status_text('test')
+            self.ids.start_button.text = 'Pause'
+            self.skip_disabled = False
 
     def update_timer(self, dt):
         self.current_time -= 1
         if self.current_time <= 0:
             self.reset_timer()
             return
-        minutes, seconds = divmod(self.current_time, 60)
-        self.timer_label = f"{minutes:02d}:{seconds:02d}"
-        self.timer_instance.set_status_text(self.timer_label)
-        self.timer_instance.set_progress(self.current_time/(self.max_time /100))
+        self.update_progress_bar()
 
     def skip_timer(self):
         self.reset_timer()
+        self.skip_disabled = True
 
     def reset_timer(self):
         if self._timer_event:
             self._timer_event.cancel()
+            self._timer_event = None
+
+        self.calculate_iterations()
+        self.set_max_time()
         self.current_time = self.max_time
+        self.update_progress_bar()
+
+        self.ids.start_button.text = 'Start'
+
+    def update_progress_bar(self):
         minutes, seconds = divmod(self.current_time, 60)
-        self.timer_label.text = f"{minutes:02d}:{seconds:02d}"
-        self.start_button.text = 'Start'
+        self.timer_label = f"{minutes:02d}:{seconds:02d}"
+        self.ids.progress_bar.set_status_text(self.timer_label)
+        self.ids.progress_bar.set_progress(100 - self.current_time/(self.max_time /100))
     
-    def on_start_pressed(self, instance):
-        self.start_timer(instance)
-        self.timer_instance = instance
+    def set_max_time(self):
+        self.max_time = self.sequence[self.iteration].get('time') * 60  # Max time in seconds
+    
+    def calculate_iterations(self):
+        self.iteration +=1
+
+        if self.iteration > self.max_iteration:
+            self.iteration = 0
